7c1539518cd52a9a687ced5988b00805d6151faa
Evgeniy Yurchenkov <gear1995V@gmail.com>
2024-09-20 20:33:36 +0500

    add run application for modules

---
+++ stats-client/src/main/java/ru/practicum/StatsClient.java
@@ -0,0 +1,9 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+
+public class StatsClient {
+    public static void main(String[] args) {
+        SpringApplication.run(StatsClient.class, args);
+    }
+}
---
+++ stats-dto/src/main/java/ru/practicum/StatDto.java
@@ -0,0 +1,9 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+
+public class StatDto {
+    public static void main(String[] args) {
+        SpringApplication.run(StatDto.class, args);
+    }
+}
