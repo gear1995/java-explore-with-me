fd1aa7ff788252c85a6add0af8cee0592773393a
Evgeniy Yurchenkov <gear1995V@gmail.com>
2024-09-27 20:57:13 +0500

    add wrapper module stat

--- ewm-service/src/main/java/ru/practicum/EwmService.java
+++ ewm-service/src/main/java/ru/practicum/EwmService.java
@@ -3,7 +3,7 @@ package ru.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class EwmService {
     public static void main(String[] args) {
         SpringApplication.run(EwmService.class, args);
--- ewm-service/src/main/java/ru/practicum/utils/Constants.java
+++ ewm-service/src/main/java/ru/practicum/utils/Constants.java
@@ -1,10 +1,7 @@
 package ru.practicum.utils;
 
-import lombok.experimental.UtilityClass;
-
 import java.time.format.DateTimeFormatter;
 
-@UtilityClass
 public class Constants {
     public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
